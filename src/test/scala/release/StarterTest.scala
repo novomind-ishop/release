9b14b97e4f55f76998ea1028e80d5f49b6e38744
Thomas Stock <tstock@novomind.com>
2017-11-02 15:14:04 +0100

    open jenkins fuer ohne .git suffix urls repariert
    Change-Id: I71889e88d9b24fae762c641fe5a969f804e6c01d

--- src/test/scala/release/StarterTest.scala
+++ src/test/scala/release/StarterTest.scala
@@ -29,9 +29,17 @@ class StarterTest extends AssertionsForJUnit {
   @Test
   def testTransformRemoteToBuildUrl_paypal(): Unit = {
     val result = Starter.transformRemoteToBuildUrl(
-      Seq(GitRemote("origin", "ishop/ext/ext-paypal.git", "remoteType")),
+      Seq(GitRemote("origin", "ssh://someone@git-ishop.novomind.com:19418/ishop/ext/ext-paypal.git", "remoteType")),
       "https://build-ishop.novomind.com")
     Assert.assertEquals("https://build-ishop.novomind.com/job/ishop-ext-ext-paypal-tag/", result.get)
+  }
+
+  @Test
+  def testTransformRemoteToBuildUrl_core(): Unit = {
+    val result = Starter.transformRemoteToBuildUrl(
+      Seq(GitRemote("origin", "ssh://someone@git-ishop.novomind.com:19418/ishop/core/ishop-core-projects", "(fetch)")),
+      "https://build-ishop.novomind.com")
+    Assert.assertEquals("https://build-ishop.novomind.com/job/ishop-core-ishop-core-projects-tag/", result.get)
   }
 
   @Test
